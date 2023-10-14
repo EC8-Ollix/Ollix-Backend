849ac373ca13945ed14036ccf8c225ffbba8b1c5
Daniel Santos De Sousa <daniel.s.6140@gmail.com>
2023-10-13 22:47:58 -0300

    OLI-17: Att script de banco

--- ollix-backend/src/Ollix.Infrastructure.Data/Scripts/AttScript.sql
+++ ollix-backend/src/Ollix.Infrastructure.Data/Scripts/AttScript.sql
@@ -1,4 +1,7 @@
-﻿CREATE TABLE [ClientApp] (
+﻿
+-- Fase 1
+
+CREATE TABLE [ClientApp] (
     [Id] uniqueidentifier NOT NULL,
     [CompanyName] nvarchar(400) NOT NULL,
     [BussinessName] nvarchar(400) NOT NULL,
@@ -31,10 +34,77 @@ CREATE TABLE [LogApp] (
     CONSTRAINT [FK_LogApp_ClientApp_ClientId] FOREIGN KEY ([ClientId]) REFERENCES [ClientApp] ([Id]) ON DELETE CASCADE
 );
 
-
 CREATE INDEX [IX_LogApp_ClientId] ON [LogApp] ([ClientId]);
 
-
 CREATE INDEX [IX_UserApp_ClientId] ON [UserApp] ([ClientId]);
+
+-- Fase 2
+
+CREATE TABLE [AddressApp] (
+    [Id] uniqueidentifier NOT NULL,
+    [PostalCode] nvarchar(16) NOT NULL,
+    [Street] nvarchar(400) NOT NULL,
+    [Number] nvarchar(16) NOT NULL,
+    [Complement] nvarchar(400) NULL,
+    [Neighborhood] nvarchar(400) NOT NULL,
+    [City] nvarchar(400) NOT NULL,
+    [State] nvarchar(400) NOT NULL,
+    CONSTRAINT [PK_AddressApp] PRIMARY KEY ([Id])
+);
+
+
+CREATE TABLE [Propeller] (
+    [Id] uniqueidentifier NOT NULL,
+    [HelixId] nvarchar(max) NULL,
+    [Active] bit NOT NULL,
+    [AddressId] uniqueidentifier NOT NULL,
+    [ClientId] uniqueidentifier NOT NULL,
+    CONSTRAINT [PK_Propeller] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Propeller_AddressApp_AddressId] FOREIGN KEY ([AddressId]) REFERENCES [AddressApp] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_Propeller_ClientApp_ClientId] FOREIGN KEY ([ClientId]) REFERENCES [ClientApp] ([Id]) ON DELETE CASCADE
+);
+
+
+CREATE TABLE [PropellerInfoDate] (
+    [Id] uniqueidentifier NOT NULL,
+    [TotalRpm] int NOT NULL,
+    [TotalKwh] int NOT NULL,
+    [ReadingCount] int NOT NULL,
+    [InfoDate] datetimeoffset NOT NULL,
+    [PropellerId] uniqueidentifier NOT NULL,
+    CONSTRAINT [PK_PropellerInfoDate] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_PropellerInfoDate_Propeller_PropellerId] FOREIGN KEY ([PropellerId]) REFERENCES [Propeller] ([Id]) ON DELETE CASCADE
+);
+
+
+CREATE TABLE [Order] (
+    [Id] uniqueidentifier NOT NULL,
+    [RequesterName] nvarchar(200) NOT NULL,
+    [RequesterEmail] nvarchar(200) NOT NULL,
+    [Observation] nvarchar(600) NULL,
+    [RequestDate] datetimeoffset NOT NULL,
+    [OrderStatus] int NOT NULL,
+    [QuantityRequested] int NOT NULL,
+    [AddressId] uniqueidentifier NOT NULL,
+    [PropellerId] uniqueidentifier NULL,
+    [ClientId] uniqueidentifier NOT NULL,
+    CONSTRAINT [PK_Order] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Order_AddressApp_AddressId] FOREIGN KEY ([AddressId]) REFERENCES [AddressApp] ([Id]) ON DELETE NO ACTION,
+    CONSTRAINT [FK_Order_ClientApp_ClientId] FOREIGN KEY ([ClientId]) REFERENCES [ClientApp] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_Order_Propeller_PropellerId] FOREIGN KEY ([PropellerId]) REFERENCES [Propeller] ([Id]) ON DELETE NO ACTION
+);
+
+
+CREATE INDEX [IX_Order_AddressId] ON [Order] ([AddressId]);
+
+CREATE INDEX [IX_Order_ClientId] ON [Order] ([ClientId]);
+
+CREATE INDEX [IX_Order_PropellerId] ON [Order] ([PropellerId]);
+
+CREATE INDEX [IX_Propeller_AddressId] ON [Propeller] ([AddressId]);
+
+CREATE INDEX [IX_Propeller_ClientId] ON [Propeller] ([ClientId]);
+
+CREATE INDEX [IX_PropellerInfoDate_PropellerId] ON [PropellerInfoDate] ([PropellerId]);
 
 
